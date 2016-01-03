a67764a2b69cd1431afcbfdaa8a0fd10d1e5f9fe
Dong Xu <asqhaqs@163.com>
2016-01-04 07:31:02 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  #root 'static_pages#home'
+  #get 'help' => 'static_pages#help'
+  #get 'about' => 'static_pages#about'
+  #get 'contact' => 'static_pages#contact'
+
+
+  root :to => 'static_pages#home'
+  get '/help',  to:'static_pages#help'
+
+  match '/about', to:'static_pages#about', via:'get'
+
+  match '/contact',to: 'static_pages#contact', via: 'get'
+  match '/signup', to: 'users#new', via:'get'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
